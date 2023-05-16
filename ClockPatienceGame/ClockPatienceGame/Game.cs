a854473e508dd9bfb379ea8243f9042d7c7066ac
izaak buttigieg <izaakbuttigieg00@gmail.com>
2023-05-16 18:51:00 +0200

    Cleaned the code. Made it more Object Oriented.
    This is the final result of the code. Cleaned everything in functions and added a new class for Game.

---
+++ ClockPatienceGame/ClockPatienceGame/Game.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockPatienceGame
+{
+    class Game
+    {
+        private List<Stack<string>> piles;
+        private int exposedCards;
+        private string currentCard;
+
+        public Game(List<string> decks)
+        {
+            piles = new List<Stack<string>>();
+            exposedCards = 0;
+
+            // Create the initial piles
+            for (int i = 0; i < 13; i++)
+                piles.Add(new Stack<string>());
+
+            // Distribute cards to piles
+            foreach (string deck in decks)
+            {
+                string[] cards = deck.Split(' ');
+
+                Array.Reverse(cards); // Reverse the order of the cards
+                // had an issue when not reversing the order of the cards
+                //as the result i was getting was 36,KD instead of 44,KD
+
+                for (int i = 0; i < 13; i++)
+                {
+                    Stack<string> currentValue = new Stack<string>();   /* this is the current card value
+                    if this was not implemented, the order of the cards will be reversed in that current
+                    pile. pile[0] will have 4 cards. Every new card will take position[0] and old card
+                    will take the next available position[1]. This would not result in the expected output*/
+
+                    while (piles[i].Count > 0)
+                    {
+                        currentValue.Push(piles[i].Pop());
+                        //pushing values in current value and popping from piles[i]
+                    }
+
+                    piles[i].Push(cards[i]); //pushing new value in piles[i]. In this instance piles will only have one value which is the new value
+
+                    while (currentValue.Count > 0)
+                    {
+                        piles[i].Push(currentValue.Pop());
+                        // populating piles once again with the old values to keep the supposed order
+                    }
+                }
+            }
+
+            currentCard = piles[12].Pop(); // Top card of the 'king' pile
+            exposedCards++;
+        }
+
+        public void Play()
+        {
+            // Play the game
+            while (true)
+            {
+                int currentPileIndex = GetPileIndex(currentCard); //gets the index form the current card that was popped from pile
+                Stack<string> currentPile = piles[currentPileIndex];
+
+                if (currentPile.Count == 0)
+                    break;
+
+                currentCard = currentPile.Pop(); // current card is removed from that pile
+                exposedCards++;
+            }
+        }
+
+        public void OutputResult()
+        {
+            // Output the result
+            Console.WriteLine($"{exposedCards:D2},{currentCard}");
+        }
+
+        private int GetPileIndex(string card)
+        {
+            char rank = card[0];
+            switch (rank)
+            {
+                case 'A': return 0;
+                case '2': return 1;
+                case '3': return 2;
+                case '4': return 3;
+                case '5': return 4;
+                case '6': return 5;
+                case '7': return 6;
+                case '8': return 7;
+                case '9': return 8;
+                case 'T': return 9;
+                case 'J': return 10;
+                case 'Q': return 11;
+                case 'K': return 12;
+                default: return -1;
+            }
+        }
+    }
+}
--- ClockPatienceGame/ClockPatienceGame/Program.cs
+++ ClockPatienceGame/ClockPatienceGame/Program.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ClockPatienceGame;
 
-class ClockPatience
+class Program
 {
     static void Main()
     {
+        // with the provided sample input
+        // there will be 4 decks
         List<string> decks = new List<string>();
-        // Initialize the clock piles
-        List<Stack<string>> piles = new List<Stack<string>>();
-        int exposedCards = 0;
 
         Console.WriteLine("Enter the decks of cards (separated by spaces, four lines of 13 cards each):");
 
@@ -21,76 +19,8 @@ class ClockPatience
             decks.Add(line);
         }
 
-        // Create the initial piles
-        for (int i = 0; i < 13; i++)
-            piles.Add(new Stack<string>());
-
-        // Distribute cards to piles
-        foreach (string deck in decks)
-        {
-            string[] cards = deck.Split(' ');
-
-            Array.Reverse(cards); // Reverse the order of the cards
-
-            for (int i = 0; i < 13; i++)
-            {
-                Stack<string> currentValue = new Stack<string>();
-                while (piles[i].Count > 0)
-                {
-                    currentValue.Push(piles[i].Pop()); // Remove all values from piles[i] and store them in currentValue
-                }
-
-                piles[i].Push(cards[i]); // Add the new value
-
-                while (currentValue.Count > 0)
-                {
-                    piles[i].Push(currentValue.Pop()); // Put the values from currentValue back into piles[i] to maintain the order
-                }
-            }
-        }
-
-        // Start the game
-        string currentCard = piles[12].Pop(); // Top card of the 'king' pile
-        exposedCards++;
-
-        // Play the game
-        while (true)
-        {
-            int currentPileIndex = GetPileIndex(currentCard);
-            Stack<string> currentPile = piles[currentPileIndex];
-
-            if (currentPile.Count == 0)
-                break;
-
-            currentCard = currentPile.Pop();
-            exposedCards++;
-        }
-
-        // Output the result
-        Console.WriteLine($"{exposedCards:D2},{currentCard}");
-    }
-
-
-    // Get the index of the pile corresponding to the card value
-    static int GetPileIndex(string card)
-    {
-        char rank = card[0];
-        switch (rank)
-        {
-            case 'A': return 0;
-            case '2': return 1;
-            case '3': return 2;
-            case '4': return 3;
-            case '5': return 4;
-            case '6': return 5;
-            case '7': return 6;
-            case '8': return 7;
-            case '9': return 8;
-            case 'T': return 9;
-            case 'J': return 10;
-            case 'Q': return 11;
-            case 'K': return 12;
-            default: return -1;
-        }
+        Game game = new Game(decks); // Creating an object of type game
+        game.Play();                // Play() and OutputResult() are both methods within the game class
+        game.OutputResult();
     }
 }
